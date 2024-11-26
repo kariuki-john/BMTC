7f08248aa002e657751dcca0b38b2d2f6e4c553a
kariuki-john <122348714+kariuki-john@users.noreply.github.com>
2024-11-26 13:23:58 +0300

    Delete backend directory

--- backend/routes/getApplications.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const { getApplications } = require("../controllers/applications.controller")
-const router = express.Router()
-
-router.get('/getApplications',getApplications)
-
-module.exports = router
--- backend/routes/postapplication.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const  {sendApplication } = require("../controllers/applications.controller")
-const router = express.Router()
-
-router.post('/sendApplication',sendApplication)
-
-module.exports = router
